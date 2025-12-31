065be04dc8a34def60085b6fa08e71dfcf87a283
jayakumar <jayakumar05122000@gmail.com>
2025-12-31 14:55:52 +0530

    feat:Some changes

--- backend/db/connectionMongoDb.js
+++ backend/db/connectionMongoDb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectMongoDb = async()=>{
     try {
-        await mongoose.connect("mongodb://localhost:27017/usersChatApp");
+        await mongoose.connect(process.env.MONGO_DB_URL);
         console.log('connected to Mongodb');
     } catch (error) {
         console.log('error connecting to Mongodb',error.message);
